d041aa00e884213ef7fb76d677901522d11bef75
Kosmas Raptis <keeperkosmas6@gmail.com>
2022-08-17 17:28:51 +0300

    Relicense under MIT

--- include/openphantom.h
+++ include/openphantom.h
@@ -1,20 +1,8 @@
 /*
 libopenphantom - A simple image loading and manipulation library
-Copyright (C) 2021 Kosmas Raptis
+Copyright (C) 2021-2022 Kosmas Raptis
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+This library is licensed under the MIT license. See COPYING for details
 */
 
 #ifndef OPENPHANTOM_H
